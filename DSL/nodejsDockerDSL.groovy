ed653ed4d8462734385e04e5bfb842ab7d16e8ea
Aleyda Pacheco <78930652+aleydapacheco@users.noreply.github.com>
2024-11-28 16:50:21 -0400

    Update nodejsDockerDSL.groovy
    Actualizacion de datos personales para readecuacion de proyecto

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/aleydapacheco/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('aleydapacheco')
+            node / gitConfigEmail('aleydabelenpachecoherrera1234@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('aleydapacheco/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
